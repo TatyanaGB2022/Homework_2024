abf620f35f9f36d6c507276133a5a75eea32052a
Tatyana <tatyana.kurii@mail.ru>
2024-04-15 19:07:40 +0600

    add Seminar_2:task4

---
+++ Seminar_2/Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры 
+//через запятую.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int divider = 1;
+while (number / divider >= 10) 
+{
+divider *= 10;
+}
+
+while (divider > 0)
+{
+  int digit = number / divider;
+  Console.Write(digit);
+  number %= divider;
+  divider /= 10;
+  if (divider > 0)
+  {
+    Console.Write(", ");
+  }
+}
