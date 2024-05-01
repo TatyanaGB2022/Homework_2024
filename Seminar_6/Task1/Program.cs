a699e9fc1e29a6d2def5132ba234c10c8abd5676
Tatyana <tatyana.kurii@mail.ru>
2024-05-01 14:17:49 +0600

    add Seminar_6:task1

---
+++ Seminar_6/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+void Main()
+{
+  char[] chars = { 'a', 'b', 'c', 'd', 'e', 'f' };
+  string str = ArrayToString(chars);
+  System.Console.WriteLine(str);
+
+}
+
+string ArrayToString(char[] chars)
+{
+  string str = "";
+  foreach (char letter in chars)
+    str += letter;
+  return str;
+}
+
+Main();
