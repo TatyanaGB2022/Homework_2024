07d2b3928832527c2607f5748fae4bb6734819fa
Tatyana <tatyana.kurii@mail.ru>
2024-05-01 15:14:52 +0600

    add Seminar_6:task3

---
+++ Seminar_6/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+// Пример:
+// “aBcD1ef!-” => Нет
+// “шалаш” => Да
+// “55655” => Да
+
+
+Console.Write("Введите строку : ");
+String str = Console.ReadLine();
+char[] chars = str.ToCharArray();
+{
+if (IsPalindrom(chars))
+  Console.WriteLine("Палиндром");
+else
+  Console.WriteLine("Не палиндром");
+        }
+        static bool IsPalindrom(char[] word)
+{
+  for (int i = 0; i < word.Length / 2; i++)
+    if (word[i] != word[word.Length - 1 - i])
+      return false;
+  return true;
+}
+    
