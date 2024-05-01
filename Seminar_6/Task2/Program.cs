37ed33f1eefe24a1000968a4fb095edb24e3e323
Tatyana <tatyana.kurii@mail.ru>
2024-05-01 14:40:45 +0600

    add Seminar_6:task2

---
+++ Seminar_6/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все 
+// заглавные буквы заменены на строчные.
+// Пример: “aBcD1ef!-” => “abcd1ef!-” 
+
+
+void Main()
+{
+  System.Console.Write("Введите строку: "); 
+  string? str = Console.ReadLine()?.ToLower(); 
+  System.Console.WriteLine(str);
+}
+
+Main();
