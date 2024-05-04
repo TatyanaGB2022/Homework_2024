aaac150d1b73e4791bcff747b4b659690e80b112
Tatyana <tatyana.kurii@mail.ru>
2024-05-04 15:01:27 +0600

    add Seminar_7:task3

---
+++ Seminar_7/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+// Пример: [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+
+void Main()
+{
+  int[] num = { 1, 2, 5, 0, 10, 34 };
+  PrintArray(num, num.Length - 1);
+}
+
+void PrintArray(int[] A, int n)
+{
+  if (n > 0)
+  {
+    System.Console.Write(A[n] + " ");
+    PrintArray(A, n - 1);
+  }
+
+  else
+    System.Console.WriteLine(A[0]);
+}
+
+Main();
+
